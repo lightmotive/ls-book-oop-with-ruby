41cffda5652aae73205f0782fb3351722661c734
Andre Light <andrelight.al@gmail.com>
2022-03-15 23:00:52 +0000

    Extend program to use all attr_* methods :-)

--- 01_the_object_model/chocolate.rb
+++ 01_the_object_model/chocolate.rb
@@ -6,14 +6,32 @@ require_relative 'eatable'
 class Chocolate
   include Eatable
 
+  # Read/Write (type, grams)
   attr_accessor :type, :grams
+  # Read-only (cacao_percent):
+  attr_reader :cacao_percent
+  # Write-only (zen_state_of_mind):
+  attr_writer :zen_state_of_mind
 
   def initialize(type, grams)
     @type = type
     @grams = grams
+
+    init_calculate
+  end
+
+  def init_calculate
+    @cacao_percent =
+      case @type.downcase
+      when 'dark' then 70
+      else 0
+      end
   end
 
   def consider
-    "Your desire for a #{grams}g #{@type} embrace could soon be fulfilled..."
+    "Your desire for a #{@grams}g #{@type} (#{@cacao_percent}%) embrace " \
+    "could soon be fulfilled...are you sure you don't want a higher " \
+    "percentage?\n" \
+    "--#{@zen_state_of_mind}--"
   end
 end
--- 01_the_object_model/do.rb
+++ 01_the_object_model/do.rb
@@ -3,5 +3,6 @@
 require_relative 'chocolate'
 
 chocolate = Chocolate.new('dark', 28)
+chocolate.zen_state_of_mind = "I'm ready!"
 puts chocolate.consider
 chocolate.savor
