495bd66657deb0496d2a4e49fb7388a5a23ba4cc
Andre Light <andrelight.al@gmail.com>
2022-03-18 17:27:05 +0000

    Improve instance variable usage with `private`
    - It's a best practice to reference instance vars in up to 2 methods:
      - 1 method for reading and 1 for writing.
    - It's OK to make auto-generated methods private.
      - That simplifies code now.
      - It also simplifies potential future override methods.

--- 01_the_object_model/chocolate.rb
+++ 01_the_object_model/chocolate.rb
@@ -1,38 +1,46 @@
-# How do we create an object in Ruby? Give an example of the creation of an
-# object.
+# frozen_string_literal: true
 
 require_relative 'eatable'
 
 class Chocolate
   include Eatable
 
-  # Read/Write (type, grams)
-  attr_accessor :type, :grams
-  # Read-only (cacao_percent):
+  # Read/Write `type`, `grams`, and `zen_state_of_mind`:
+  attr_accessor :type, :grams, :zen_state_of_mind
+  # Make the `zen_state_of_mind` reader method private:
+  private :zen_state_of_mind
+  # If one wanted to make the generated `zen_state_of_mind` writer method
+  # private, they could change the line above to:
+  # `private :zen_state_of_mind, :zen_state_of_mind=`
+
+  # Read-only ivar `cacao_percent` (custom update via `update_cacao_percent`):
   attr_reader :cacao_percent
-  # Write-only (zen_state_of_mind):
-  attr_writer :zen_state_of_mind
 
   def initialize(type, grams)
-    @type = type
-    @grams = grams
+    # Save `type` and `grams` params to associated ivars:
+    self.type = type
+    self.grams = grams
 
-    init_calculate
+    update_cacao_percent
   end
 
-  def init_calculate
+  def update_cacao_percent
+    # `cacao_percent` will be updated only in this method; update the ivar here:
     @cacao_percent =
-      case @type.downcase
+      case type.downcase
       when 'dark' then 70
       when 'very dark' then 85
       else 0
       end
   end
+  # Make `update_cacao_percent` private:
+  private :update_cacao_percent
 
   def consider
-    "Your desire for a #{@grams}g #{@type} (#{@cacao_percent}%) embrace " \
+    # Use `grams`, `type`, `cacao_percent`, and `zen_state_of_mind` ivars:
+    "Your desire for a #{grams}g #{type} (#{cacao_percent}%) embrace " \
     "could soon be fulfilled...are you sure you don't want a higher " \
     "percentage?\n" \
-    "--#{@zen_state_of_mind}--"
+    "--#{zen_state_of_mind}--"
   end
 end
--- 01_the_object_model/do.rb
+++ 01_the_object_model/do.rb
@@ -3,6 +3,6 @@
 require_relative 'chocolate'
 
 chocolate = Chocolate.new('very dark', 28)
-chocolate.zen_state_of_mind = "I'm ready!"
+chocolate.zen_state_of_mind = '..chocolate..'
 puts chocolate.consider
 chocolate.savor
